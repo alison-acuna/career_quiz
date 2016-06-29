f93ac514a23a7293079c3911830c68a4e1fb2974
alison-acuna <aacuna136@gmail.com>
2016-06-29 16:27:01 -0700

    Initial Logic done.  NS:  Fix tally count

--- program.js
+++ program.js
@@ -4,43 +4,77 @@ $(document).ready(function(){
 
 var extrovert_score = 0
 var design_score = 0
-var data_score = 0
 var qa_score = 0
+var data_score = 0
 
 $("#clientquestion").submit(function(event){
   event.preventDefault();
+  $("#clientquestion").toggle(display= false);
   if(document.getElementById('yes').checked){
-      $("#clientquestion").toggle(display= false);
       $("#designquestion").toggle(display= true);
       extrovert_score +=2
-      console.log(extrovert_score)
   }else if(document.getElementById('no').checked){
-    $("#clientquestion").toggle(display= false);
     $("#designquestion").toggle(display= true);
-    console.log(extrovert_score)
   } else {
-    $("#clientquestion").toggle(display= false);
     $("#designquestion").toggle(display= true);
     extrovert_score +=1
-    console.log(extrovert_score)
   }
+  console.log("extrovert "+ extrovert_score)
 });
 
 $("#designquestion").submit(function(event){
   event.preventDefault();
+  $("#designquestion").toggle(display= false);
   if(document.getElementById('yes').checked){
-      $("#designquestion").toggle(display= false);
       $("#editquestion").toggle(display= true);
       design_score +=2
-      console.log(extrovert_score)
   }else if(document.getElementById('no').checked){
-    $("#designquestion").toggle(display= false);
     $("#editquestion").toggle(display= true);
-    console.log(extrovert_score)
   } else {
-    $("#designquestion").toggle(display= false);
     $("#editquestion").toggle(display= true);
     design_score +=1
-    console.log(extrovert_score)
   }
+  console.log("design " + design_score)
+});
+
+$("#editquestion").submit(function(event){
+  event.preventDefault();
+  $("#editquestion").toggle(display= false);
+  if(document.getElementById('yes').checked){
+      $("#databasequestion").toggle(display= true);
+      qa_score +=2
+  }else if(document.getElementById('no').checked){
+    $("#databasequestion").toggle(display= true);
+    console.log(extrovert_score)
+  } else {
+    $("#databasequestion").toggle(display= true);
+    qa_score +=1
+  }
+  console.log("QA " + qa_score)
+});
+
+$("#databasequestion").submit(function(event){
+  event.preventDefault();
+  $("#databasequestion").toggle(display= false);
+  if(document.getElementById('yes').checked){
+      $("#partyquestion").toggle(display= true);
+      data_score +=2
+  }else if(document.getElementById('na').checked){
+    data_score +=1
+  } else if(document.getElementById('na').checked){
+    // display resources
+  }
+  console.log("data " + data_score)
+});
+
+$("#partyquestion").submit(function(event){
+  event.preventDefault();
+  $("#partyquestion").toggle(display= false);
+  if(document.getElementById('yes').checked){
+      extrovert_score +=2
+  }else if(document.getElementById('no').checked){
+  } else {
+    extrovert_score +=1
+  }
+  console.log("extrovert "+ extrovert_score)
 });
