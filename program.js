1a9d610f7fd8ec545c01839b731d7fbe4f36055c
alison-acuna <aacuna136@gmail.com>
2016-06-30 14:34:03 -0700

    Tally count finished.  NS: add questions or idk functionality

--- program.js
+++ program.js
@@ -10,71 +10,88 @@ var data_score = 0
 $("#clientquestion").submit(function(event){
   event.preventDefault();
   $("#clientquestion").toggle(display= false);
-  if(document.getElementById('yes').checked){
-      $("#designquestion").toggle(display= true);
-      extrovert_score +=2
-  }else if(document.getElementById('no').checked){
+  if(document.getElementById('clientyes').checked){
+    $("#designquestion").toggle(display= true);
+    extrovert_score +=2;
+  }else if(document.getElementById('clientno').checked){
     $("#designquestion").toggle(display= true);
   } else {
     $("#designquestion").toggle(display= true);
-    extrovert_score +=1
+    extrovert_score +=1;
   }
-  console.log("extrovert "+ extrovert_score)
+  console.log("extrovert "+ extrovert_score);
+  console.log("design " + design_score);
+  console.log("QA " + qa_score);
+  console.log("Data Score " + data_score);
 });
 
 $("#designquestion").submit(function(event){
   event.preventDefault();
   $("#designquestion").toggle(display= false);
-  if(document.getElementById('yes').checked){
-      $("#editquestion").toggle(display= true);
-      design_score +=2
-  }else if(document.getElementById('no').checked){
+  if(document.getElementById('designyes').checked){
+    design_score += 2;
     $("#editquestion").toggle(display= true);
+  }else if(document.getElementById('designno').checked){
+    $("#editquestion").toggle(display= true);
+    design_score = 0;
   } else {
     $("#editquestion").toggle(display= true);
-    design_score +=1
+    design_score +=1;
   }
-  console.log("design " + design_score)
+  console.log("extrovert "+ extrovert_score);
+  console.log("design " + design_score);
+  console.log("QA " + qa_score);
+  console.log("Data Score " + data_score);
 });
 
 $("#editquestion").submit(function(event){
   event.preventDefault();
   $("#editquestion").toggle(display= false);
-  if(document.getElementById('yes').checked){
-      $("#databasequestion").toggle(display= true);
-      qa_score +=2
-  }else if(document.getElementById('no').checked){
+  if(document.getElementById('edityes').checked){
+    $("#databasequestion").toggle(display= true);
+    qa_score +=2
+  }else if(document.getElementById('editno').checked){
     $("#databasequestion").toggle(display= true);
     console.log(extrovert_score)
   } else {
     $("#databasequestion").toggle(display= true);
     qa_score +=1
   }
-  console.log("QA " + qa_score)
+  console.log("extrovert "+ extrovert_score);
+  console.log("design " + design_score);
+  console.log("QA " + qa_score);
+  console.log("Data Score " + data_score);
 });
 
 $("#databasequestion").submit(function(event){
   event.preventDefault();
   $("#databasequestion").toggle(display= false);
-  if(document.getElementById('yes').checked){
-      $("#partyquestion").toggle(display= true);
-      data_score +=2
-  }else if(document.getElementById('na').checked){
+  if(document.getElementById('databaseyes').checked){
+    $("#partyquestion").toggle(display= true);
+    data_score +=2
+  }else if(document.getElementById('databasena').checked){
     data_score +=1
-  } else if(document.getElementById('na').checked){
+  } else if(document.getElementById('databaseidk').checked){
     // display resources
   }
-  console.log("data " + data_score)
+  console.log("extrovert "+ extrovert_score);
+  console.log("design " + design_score);
+  console.log("QA " + qa_score);
+  console.log("Data Score " + data_score);
+
 });
 
 $("#partyquestion").submit(function(event){
   event.preventDefault();
   $("#partyquestion").toggle(display= false);
-  if(document.getElementById('yes').checked){
-      extrovert_score +=2
-  }else if(document.getElementById('no').checked){
+  if(document.getElementById('partyyes').checked){
+    extrovert_score +=2
+  }else if(document.getElementById('partyno').checked){
   } else {
     extrovert_score +=1
   }
-  console.log("extrovert "+ extrovert_score)
+  console.log("extrovert "+ extrovert_score);
+  console.log("design " + design_score);
+  console.log("QA " + qa_score);
+  console.log("Data Score " + data_score);
 });
