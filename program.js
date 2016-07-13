1c56c1974769ede2c2ddf88495b8aa01f38411ee
alison-acuna <aacuna136@gmail.com>
2016-07-13 14:18:42 -0700

    Added chart. Make available online

--- program.js
+++ program.js
@@ -108,16 +108,16 @@ $("#frontquestion").submit(function(event){
   }
 });
 
+
 $("#finaltally").submit(function(event){
   event.preventDefault();
+  console.log(designer)
   var designer = design_score + 2
   var front_end = design_score + code_score
   var back_end = qa_score + code_score
   var tech_support = extrovert_score + code_score
   var database = data_score + 2
   var qa = qa_score + 2
-
-  console.log(designer)
   if (designer >= qa &&
       designer >= database &&
       designer >= front_end &&
@@ -158,12 +158,43 @@ $("#finaltally").submit(function(event){
     console.log("test functioned")
   }
   document.getElementById('deepresults').innerHTML = "Here are your scores.  If you have some other high scoring options, consider exploring those as well!"
-  document.getElementById('design').innerHTML = " Design Score: " + designer
-  document.getElementById('qa').innerHTML = " QA Score: " + qa
-  document.getElementById('db').innerHTML = " Data Score: " + database
-  document.getElementById('frontend').innerHTML = " Front End Score: " + front_end
+
+  var ctx = document.getElementById("myChart");
+
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ["Design", "Front End", "Full Stack", "Tech Support", "QA", "Database"],
+          datasets: [{
+              label: 'Results',
+              data: [designer, front_end, back_end, tech_support, database, qa],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
 });
-
-
-// NS:  Rearrange/add more questions + develop front end
-// Refactor:  Suggest multiple career paths.
